7c31b08ab3fba080aa977fe265e960eaa65c4c11
Kirill Belyaev <kirill10121415@gmail.com>
2024-12-08 02:39:29 +0300

    edit class Graph fields edit class Graph Constructor

--- graph/graph.go
+++ graph/graph.go
@@ -1,14 +1,31 @@
 package graph
 
+//
+//type Node struct {
+//	edges []*Edge
+//}
+//
+//type Edge struct {
+//	start, end *Node
+//	weight     int
+//}
+
 type Graph struct {
-	V   int
-	adj [][]int
+	V               int
+	nodes           []string
+	adjacencyMatrix [][]int
 }
 
-func NewGraph(V int) *Graph {
-	return &Graph{V: V, adj: make([][]int, V)} // непонятно
+func NewGraph(V int, nodes []string, matrix [][]int) *Graph {
+	if nodes == nil {
+		nodes = make([]string, V)
+	}
+	if matrix == nil {
+		matrix = make([][]int, V)
+	}
+	return &Graph{V: V, nodes: nodes, adjacencyMatrix: matrix} // непонятно
 }
 
-func (graph *Graph) AddEdge(v, w int) {
-	graph.adj[v] = append(graph.adj[v], w)
-}
+//func (graph *Graph) AddEdge(v, w int) {
+//	graph.edges[v] = append(graph.edges[v], w)
+//}
