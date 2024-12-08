cd9f54752300832f8c322eb809ec2d86032b424e
Kirill Belyaev <kirill10121415@gmail.com>
2024-12-08 15:20:16 +0300

    + GetSrc() and GetDest() Edge's method's + ToAdjacencyMatrix() Graph's method to make adjacency matrix graph's view + ToAdjacencyLists() Graph's method to make adjacency lists graph's view + ToIncidenceMatrix() Graph's method to make incidence matrix graph's view
    edited Graph class Constructor

--- graph/graph.go
+++ graph/graph.go
@@ -1,12 +1,14 @@
 package graph
 
+import "strconv"
+
 type Node struct {
-	name  string
+	Name  string
 	edges []*Edge
 }
 
 func NewNode(name string) *Node {
-	return &Node{name: name}
+	return &Node{Name: name}
 }
 
 type Edge struct {
@@ -18,50 +20,52 @@ func NewEdge(src, dest *Node, weight int) *Edge {
 	return &Edge{src, dest, weight}
 }
 
+func (edge *Edge) GetSrc() *Node {
+	return edge.src
+}
+
+func (edge *Edge) GetDest() *Node {
+	return edge.dest
+}
+
 type Graph struct {
 	nodesAmount int
 	nodes       []*Node
 	edges       []*Edge
 }
 
-func NewGraph(V int, nodes []string, matrix [][]int) *Graph {
-	if nodes == nil {
-		nodes = make([]string, V)
-	}
-	if matrix == nil {
-		matrix = make([][]int, V)
-	}
-	return &Graph{V: V, nodes: nodes, adjacencyMatrix: matrix} // непонятно
+func NewGraph() *Graph {
+	return &Graph{nodesAmount: 0, nodes: nil, edges: nil}
 }
 
 func (graph *Graph) containsNode(nodeName string) int {
 	for i, node := range graph.nodes {
-		if node.name == nodeName {
+		if node.Name == nodeName {
 			return i
 		}
 	}
 	return -1
 }
 
-func (graph *Graph) containsEdge(src, dest *Node, weight int) int {
+func (graph *Graph) containsEdge(src, dest *Node) int {
 
 	for i, edge := range graph.edges {
 		if (edge.src == src && edge.dest == dest) || (edge.src == dest && edge.dest == src) {
-			if edge.weight == weight {
-				return i
-			}
+			return i
 		}
 	}
 	return -1
 }
 
 func (graph *Graph) AddEdge(src, dest string, weight int) {
-
+	if weight == 0 {
+		return
+	}
 	srcInd := graph.containsNode(src)
 	destInd := graph.containsNode(dest)
 
 	if srcInd != -1 && destInd != -1 { // 2 ноды уже существуют
-		if graph.containsEdge(graph.nodes[srcInd], graph.nodes[destInd], weight) == -1 {
+		if graph.containsEdge(graph.nodes[srcInd], graph.nodes[destInd]) == -1 {
 			edge := NewEdge(graph.nodes[srcInd], graph.nodes[destInd], weight)
 			graph.edges = append(graph.edges, edge)
 			graph.nodes[srcInd].edges = append(graph.nodes[srcInd].edges, edge)
@@ -77,6 +81,7 @@ func (graph *Graph) AddEdge(src, dest string, weight int) {
 		graph.nodes = append(graph.nodes, srcNode)
 		graph.nodes = append(graph.nodes, destNode)
 		graph.edges = append(graph.edges, edge)
+		graph.nodesAmount += 2
 
 	} else if srcInd == -1 { // только одна нода существует
 		srcNode := NewNode(src)
@@ -85,6 +90,7 @@ func (graph *Graph) AddEdge(src, dest string, weight int) {
 		graph.nodes[destInd].edges = append(graph.nodes[destInd].edges, edge)
 		graph.edges = append(graph.edges, edge)
 		graph.nodes = append(graph.nodes, srcNode)
+		graph.nodesAmount++
 
 	} else {
 		destNode := NewNode(dest)
@@ -93,5 +99,61 @@ func (graph *Graph) AddEdge(src, dest string, weight int) {
 		graph.nodes[srcInd].edges = append(graph.nodes[srcInd].edges, edge)
 		graph.edges = append(graph.edges, edge)
 		graph.nodes = append(graph.nodes, destNode)
+		graph.nodesAmount++
 	}
+}
+
+func (graph *Graph) ToAdjacencyMatrix() ([]string, [][]int) {
+	headers := make([]string, graph.nodesAmount)
+	matrix := make([][]int, graph.nodesAmount)
+	for i := 0; i < graph.nodesAmount; i++ {
+		matrix[i] = make([]int, graph.nodesAmount)
+	}
+
+	for i, src := range graph.nodes {
+		headers[i] = src.Name
+		for j, dest := range graph.nodes {
+			edgeInd := graph.containsEdge(src, dest)
+			if edgeInd != -1 {
+				matrix[i][j] = graph.edges[edgeInd].weight
+			}
+		}
+	}
+	return headers, matrix
+}
+
+func (graph *Graph) ToAdjacencyLists() []string {
+	adjLists := make([]string, graph.nodesAmount)
+	var edgeStr string
+	var addEdge *Node
+	for i, node := range graph.nodes {
+		edgeStr = node.Name + ": "
+		for _, edge := range node.edges {
+			if edge.src == node {
+				addEdge = edge.dest
+			} else {
+				addEdge = edge.src
+			}
+			edgeStr += addEdge.Name + "(" + strconv.Itoa(edge.weight) + "), "
+		}
+		adjLists[i] = edgeStr[:len(edgeStr)-2]
+	}
+	return adjLists
+}
+
+func (graph *Graph) ToIncidenceMatrix() ([]*Edge, []*Node, [][]int) {
+	matrix := make([][]int, len(graph.edges))
+	for i := range matrix {
+		matrix[i] = make([]int, graph.nodesAmount)
+	}
+
+	for i, edge := range graph.edges {
+		for j, node := range graph.nodes {
+			if edge.src == node || edge.dest == node {
+				matrix[i][j] = 1
+			}
+		}
+	}
+
+	return graph.edges, graph.nodes, matrix
 }
