5db630fc3ef032537adeee56b8ffa65a8f7b4406
Kirill Belyaev <kirill10121415@gmail.com>
2024-12-08 04:33:10 +0300

    edit class graph architecture add class Node + constructor add class Edge + constructor add function AddEdge()

--- graph/graph.go
+++ graph/graph.go
@@ -1,19 +1,27 @@
 package graph
 
-//
-//type Node struct {
-//	edges []*Edge
-//}
-//
-//type Edge struct {
-//	start, end *Node
-//	weight     int
-//}
+type Node struct {
+	name  string
+	edges []*Edge
+}
+
+func NewNode(name string) *Node {
+	return &Node{name: name}
+}
+
+type Edge struct {
+	src, dest *Node
+	weight    int
+}
+
+func NewEdge(src, dest *Node, weight int) *Edge {
+	return &Edge{src, dest, weight}
+}
 
 type Graph struct {
-	V               int
-	nodes           []string
-	adjacencyMatrix [][]int
+	nodesAmount int
+	nodes       []*Node
+	edges       []*Edge
 }
 
 func NewGraph(V int, nodes []string, matrix [][]int) *Graph {
@@ -26,6 +34,64 @@ func NewGraph(V int, nodes []string, matrix [][]int) *Graph {
 	return &Graph{V: V, nodes: nodes, adjacencyMatrix: matrix} // непонятно
 }
 
-//func (graph *Graph) AddEdge(v, w int) {
-//	graph.edges[v] = append(graph.edges[v], w)
-//}
+func (graph *Graph) containsNode(nodeName string) int {
+	for i, node := range graph.nodes {
+		if node.name == nodeName {
+			return i
+		}
+	}
+	return -1
+}
+
+func (graph *Graph) containsEdge(src, dest *Node, weight int) int {
+
+	for i, edge := range graph.edges {
+		if (edge.src == src && edge.dest == dest) || (edge.src == dest && edge.dest == src) {
+			if edge.weight == weight {
+				return i
+			}
+		}
+	}
+	return -1
+}
+
+func (graph *Graph) AddEdge(src, dest string, weight int) {
+
+	srcInd := graph.containsNode(src)
+	destInd := graph.containsNode(dest)
+
+	if srcInd != -1 && destInd != -1 { // 2 ноды уже существуют
+		if graph.containsEdge(graph.nodes[srcInd], graph.nodes[destInd], weight) == -1 {
+			edge := NewEdge(graph.nodes[srcInd], graph.nodes[destInd], weight)
+			graph.edges = append(graph.edges, edge)
+			graph.nodes[srcInd].edges = append(graph.nodes[srcInd].edges, edge)
+			graph.nodes[destInd].edges = append(graph.nodes[destInd].edges, edge)
+		}
+
+	} else if srcInd == -1 && destInd == -1 { // обеих нод не существует
+		srcNode := NewNode(src)
+		destNode := NewNode(dest)
+		edge := NewEdge(srcNode, destNode, weight)
+		srcNode.edges = append(srcNode.edges, edge)
+		destNode.edges = append(destNode.edges, edge)
+		graph.nodes = append(graph.nodes, srcNode)
+		graph.nodes = append(graph.nodes, destNode)
+		graph.edges = append(graph.edges, edge)
+
+	} else if srcInd == -1 { // только одна нода существует
+		srcNode := NewNode(src)
+		edge := NewEdge(srcNode, graph.nodes[destInd], weight)
+		srcNode.edges = append(srcNode.edges, edge)
+		graph.nodes[destInd].edges = append(graph.nodes[destInd].edges, edge)
+		graph.edges = append(graph.edges, edge)
+		graph.nodes = append(graph.nodes, srcNode)
+
+	} else {
+		destNode := NewNode(dest)
+		edge := NewEdge(destNode, graph.nodes[srcInd], weight)
+		destNode.edges = append(destNode.edges, edge)
+		graph.nodes[srcInd].edges = append(graph.nodes[srcInd].edges, edge)
+		graph.edges = append(graph.edges, edge)
+		graph.nodes = append(graph.nodes, destNode)
+	}
+}
