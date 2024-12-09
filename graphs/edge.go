39862d49299c8aa5d51ac4e8f7f2359f91e9c704
Kirill Belyaev <kirill10121415@gmail.com>
2024-12-10 02:32:26 +0300

    method sortEdges moved from graph.gp file

--- graphs/edge.go
+++ graphs/edge.go
@@ -16,3 +16,15 @@ func (edge *Edge) GetSrc() *Node {
 func (edge *Edge) GetDest() *Node {
 	return edge.dest
 }
+
+func sortEdges(edges []*Edge) []*Edge {
+	n := len(edges)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if edges[j].weight > edges[j+1].weight {
+				edges[j], edges[j+1] = edges[j+1], edges[j]
+			}
+		}
+	}
+	return edges
+}
