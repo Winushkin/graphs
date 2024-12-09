eefa09a67b1debb0b3ec751ffa1fd2f45755163c
Kirill Belyaev <kirill10121415@gmail.com>
2024-12-10 02:31:58 +0300

    + Node's method sortNodes()

--- graphs/node.go
+++ graphs/node.go
@@ -8,3 +8,15 @@ type Node struct {
 func NewNode(name string) *Node {
 	return &Node{Name: name}
 }
+
+func sortNodes(nodes []*Node) []*Node {
+	n := len(nodes)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if nodes[j].Name > nodes[j+1].Name {
+				nodes[j], nodes[j+1] = nodes[j+1], nodes[j]
+			}
+		}
+	}
+	return nodes
+}
